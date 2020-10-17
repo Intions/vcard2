1753c8f4b894208e03a9820156f2291e3ac1c14e
Goh Thian Hock <goh@intions.com>
2020-10-17 17:15:36 +0800

    add sample of table storage

--- api/QueryAPI.cs
+++ api/QueryAPI.cs
@@ -8,6 +8,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
+using Microsoft.Azure;
+using Microsoft.Azure.Cosmos.Table;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Intions.vCard2
 {
     public static class QueryAPI
@@ -19,17 +24,57 @@ namespace Intions.vCard2
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string querystring = req.Query["Query"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
+            /*
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
+            */
+
+            var storageCredentials = new StorageCredentials("spintegratorstorage01", "N /ig5UBFyaWny2ofxIOO1MS7LGWktqJGFMlJUcfWg9zpZFiMw0yexfX/2x1G8bJT4FJXgH0GveCiCFMhF34SwQ==");
+
+            CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            var cloudTable = tableClient.GetTableReference("Submission");
+
+            try
+            {
+                TableQuery<DynamicTableEntity> DataTableQuery = new TableQuery<DynamicTableEntity>();
+                if (!string.IsNullOrEmpty(querystring))
+                {
+                    DataTableQuery = new TableQuery<DynamicTableEntity>().Where(querystring);
+                }
+                IEnumerable<DynamicTableEntity> IDataList = cloudTable.ExecuteQuery(DataTableQuery);
+                List<DynamicTableEntity> DataList =  IDataList.ToList<DynamicTableEntity>();
+
+                List<IDictionary<string, object>> expando = new List<IDictionary<string, object>>();
+
+                DataList.ForEach((item) =>
+                {
+                    Dictionary<string, object> output = new Dictionary<string, object>();
+                    List<string> keys = item.Properties.Keys.ToList<string>();
+                    foreach (var key in keys)
+                    {
+                        output[key] = item.Properties[key].ToString();
+                    }
+                    expando.Add(output);
+                });
+
+                return  new OkObjectResult(expando);
+            }
+            catch (Exception ExceptionObj)
+            {
+                return new BadRequestObjectResult(ExceptionObj.Message);
+                
+            }
         }
+ 
     }
 }
